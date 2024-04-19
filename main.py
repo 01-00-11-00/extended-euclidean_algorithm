7ac9d22c8b3f55591616bd6b81ccca8d82affbcd
01-00-11-00 <l.seijo_erler@proton.me>
2024-04-19 17:30:47 +0200

    feat: Create main.py

---
+++ main.py
@@ -0,0 +1,58 @@
+
+# ---------------------------- Functions ------------------------------- #
+
+def extended_euclid_algorithm(a: int, b: int) -> tuple:
+   
+    x = 0
+    y = 1
+    x_prev, y_prev = 1, 0
+    
+    while b != 0:
+        quotient = a // b
+        a, b = b, a % b
+        x, x_prev = x_prev - quotient * x, x
+        y, y_prev = y_prev - quotient * y, y
+        
+    return (a, x_prev, y_prev)
+
+
+# ---------------------------- Variables ------------------------------- #
+
+first_num = None
+second_num = None
+
+
+# ------------------------------ Main ---------------------------------- #
+
+if __name__ == "__main__":
+
+    # get valid numbers
+    while first_num is None or second_num is None:
+
+        print()
+        input_a = input("Enter the first number: ")
+        input_b = input("Enter the second number: ")
+    
+        # check for valid input
+        if input_a.isdigit() and input_b.isdigit():
+            first_num = int(input_a)
+            second_num = int(input_b)
+
+        else:
+            print()
+            print("Invalid Input!\nPlease enter two numbers.")
+
+    # sort the numbers
+    if second_num > first_num:
+        first_num, second_num = second_num, first_num 
+
+    result = extended_euclid_algorithm(first_num, second_num)
+
+    # Return the results
+    print()
+    print("Result:")
+    print("-" * 11)
+    print(f"GCD: {result[0]}")
+    print(f"{first_num}⁻¹: {result[1]}")
+    print(f"{second_num}⁻¹: {result[2]}")
+
